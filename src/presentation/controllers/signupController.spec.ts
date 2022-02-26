aacf90bd815d4387656968c574bc4ef9ddaae5ee
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-02-26 20:57:09 -0300

    feat: ensure SignUpController retruns 400 if no name is provided

---
+++ src/presentation/controllers/signupController.spec.ts
@@ -0,0 +1,22 @@
+import { SignUpController } from './signupController'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided ', () => {
+    // Arrange
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    // Act
+    const httpResponse = sut.handle(httpRequest)
+
+    // Assert
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
