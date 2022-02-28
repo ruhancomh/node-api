2db0bddf5b783fa8a933859d9b7efa818a2787d2
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-02-27 21:05:33 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -223,6 +223,32 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new InternalServerError())
   })
+
+  test('Should return 200 if valid data is provided', () => {
+    // Arrange
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    // Act
+    const httpResponse = sut.handle(httpRequest)
+
+    // Assert
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 1,
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    })
+  })
 })
 
 interface SutTypes {
