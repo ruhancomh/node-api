1d8179bdc8c7ae18abf084d904d401eea6335dc5
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-02 19:03:27 -0300

    refactor: move sut creation to method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,17 +1,31 @@
 import { AddAccountModel } from '../../../domain/protocols/add-account-model'
 import { DbAddAccount } from './db-add-account'
 
+class EncrypterStub {
+  async encrypt (value: string): Promise<string> {
+    return await new Promise(resolve => resolve('encrypted_password'))
+  }
+}
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: EncrypterStub
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut: sut,
+    encrypterStub: encrypterStub
+  }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => resolve('encrypted_password'))
-      }
-    }
-
     // Arrange
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData: AddAccountModel = {
       email: 'valid_email',
