fa6b17194af61c937168f2b0cff3c699f7667516
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-04 13:32:33 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,6 +1,8 @@
+import { AccountModel } from './../../../domain/models/account-model'
 import { AddAccountModel } from '../../../domain/protocols/add-account-model'
 import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+import { AddAccountRepository } from '../../protocols/repositories/add-account-repository'
 
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password', async () => {
@@ -38,6 +40,27 @@ describe('DbAddAccount Usecase', () => {
     // Assert
     await expect(result).rejects.toThrow()
   })
+
+  test('Should call AddAccountRepository with correct values', async () => {
+    // Arrange
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+    const accountData: AddAccountModel = {
+      email: 'valid_email',
+      name: 'valid_name',
+      password: 'valid_password'
+    }
+
+    // Act
+    await sut.add(accountData)
+
+    // Assert
+    expect(addSpy).toHaveBeenCalledWith({
+      email: 'valid_email',
+      name: 'valid_name',
+      password: 'encrypted_password'
+    })
+  })
 })
 
 class EncrypterStub implements Encrypter {
@@ -46,17 +69,33 @@ class EncrypterStub implements Encrypter {
   }
 }
 
+class AddAccountRepositoryStub implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const fakeAccount: AccountModel = {
+      id: 1,
+      email: 'valid_email',
+      name: 'valid_name',
+      password: 'encrypted_password'
+    }
+
+    return await new Promise(resolve => resolve(fakeAccount))
+  }
+}
+
 interface SutTypes {
   sut: DbAddAccount
   encrypterStub: EncrypterStub
+  addAccountRepositoryStub: AddAccountRepositoryStub
 }
 
 const makeSut = (): SutTypes => {
   const encrypterStub = new EncrypterStub()
-  const sut = new DbAddAccount(encrypterStub)
+  const addAccountRepositoryStub = new AddAccountRepositoryStub()
+  const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
 
   return {
     sut: sut,
-    encrypterStub: encrypterStub
+    encrypterStub: encrypterStub,
+    addAccountRepositoryStub: addAccountRepositoryStub
   }
 }
