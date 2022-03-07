b043e613b10206cff08f8c8b2fa8feb9b01d021d
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-07 20:39:55 -0300

    refactor: create makeSut method

--- src/infra/db/mongodb/repositories/account/account-repository.spec.ts
+++ src/infra/db/mongodb/repositories/account/account-repository.spec.ts
@@ -1,3 +1,4 @@
+import { AddAccountRepository } from './../../../../../data/protocols/repositories/add-account-repository'
 import { AccountMongoRepository } from './account-repository'
 import { MongoHelper } from './../../helpers/mongo-helper'
 import { IAccountModel } from './../../../../../domain/models/account-model'
@@ -14,7 +15,7 @@ describe('Account Mongo Repository', () => {
 
   test('Should return an account on success', async () => {
     // Arrange
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const accountData: AddAccountModel = {
       name: 'any_name',
       email: 'any_email',
@@ -32,3 +33,7 @@ describe('Account Mongo Repository', () => {
     expect(account.password).toBe('any_password')
   })
 })
+
+const makeSut = (): AddAccountRepository => {
+  return new AccountMongoRepository()
+}
