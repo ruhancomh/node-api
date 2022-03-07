8f393a7cd4fb3bf5f1e1d67a792b1734525ca8a5
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-07 20:35:41 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/repositories/account/account-repository.spec.ts
@@ -0,0 +1,34 @@
+import { AccountMongoRepository } from './account-repository'
+import { MongoHelper } from './../../helpers/mongo-helper'
+import { IAccountModel } from './../../../../../domain/models/account-model'
+import { AddAccountModel } from './../../../../../domain/protocols/add-account-model'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.close()
+  })
+
+  test('Should return an account on success', async () => {
+    // Arrange
+    const sut = new AccountMongoRepository()
+    const accountData: AddAccountModel = {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
+
+    // Act
+    const account: IAccountModel = await sut.add(accountData)
+
+    // Assert
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
+  })
+})
