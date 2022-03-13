aecdba8606d71830533e315247449daa233366fa
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-13 18:48:50 -0300

    refactor: ensure integration tests connect to db

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterEach(async () => {
+    await MongoHelper.clear()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.close()
+  })
+
   test('Should return an account on success', async () => {
     // Arrange
     const signUpData = {
