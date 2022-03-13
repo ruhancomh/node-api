f271250b708389c8529457f5875e4d25949a4a0f
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-03-13 18:35:08 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import { setupMiddlewares } from './middlewares'
 import express from 'express'
+import { setupRouts } from './routs'
 
 const app = express()
 setupMiddlewares(app)
+setupRouts(app)
 
 export default app
